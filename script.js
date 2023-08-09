3476ebef9afd0bce8be3b8e399b5233816047f15
MdMomen1 <25047920-MdMomen1@users.noreply.replit.com>
2023-08-09 14:25:55 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,21 @@
+
+
+/*
+TODO:
+  - [X] Navbar
+    - [X] Responsive 
+
+  - [X] Gamejoy home top
+    - [X] Fix up the position 
+    - [X] Fix up the logo/main (make it better)
+    - [] Responsive
+
+  - [X] Origin section
+    - [] Responsive
+
+  - [X] Contact section
+    - [] Responsive
+
+  - [X] Footer 
+    - [] Responsive
+*/
